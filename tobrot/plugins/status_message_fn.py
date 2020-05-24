92a845e0dd1282a42a8d98ae458db6a408d87af8
Jerryj1127 <57258070+Jerryj1127@users.noreply.github.com>
2020-05-24 15:33:33 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -84,7 +84,7 @@ async def cancel_message_f(client, message):
             LOGGER.info(downloads)
             LOGGER.info(downloads.remove(force=True))
             await i_m_s_e_g.edit_text(
-                "Leech Cancelled"
+                "Food got cancelled. I'm hungry"
             )
         except Exception as e:
             await i_m_s_e_g.edit_text(
