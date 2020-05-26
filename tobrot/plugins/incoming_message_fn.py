c0abc4d84828e5c261657668fc089322adfd1403
Jerryj1127 <57258070+Jerryj1127@users.noreply.github.com>
2020-05-26 23:21:27 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -80,7 +80,7 @@ async def incoming_message_f(client, message):
             await i_m_sefg.edit_text(err_message)
     else:
         await i_m_sefg.edit_text(
-            "Nah, ethu thenga?. \nPlease read /help \n"
+            "**enthu tholviyado**! \n reply to a magnet or any other link \n"
             f"<b>API Error</b>: {cf_name}"
         )
 
@@ -126,6 +126,6 @@ async def incoming_youtube_dl_f(client, message):
             )
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "**enthu tholviyado**! \n reply to a youtube or any other video link \n"
             f"<b>API Error</b>: {cf_name}"
         )
