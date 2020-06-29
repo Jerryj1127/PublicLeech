9d349e0e8ee92ccbc25e8484a1e7ec97bbd78c09
Jerryj1127 <57258070+Jerryj1127@users.noreply.github.com>
2020-06-29 19:30:51 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -3,6 +3,14 @@
 # (c) Shrimadhav U K
 
 # the logging things
+import ast 
+from random import randint
+from os import path, remove
+from csv import reader
+from re import search 
+from requests import get
+from zipfile import ZipFile
+
 import logging
 logging.basicConfig(
     level=logging.DEBUG,
@@ -17,6 +25,61 @@ import pyrogram
 from tobrot import (
     AUTH_CHANNEL
 )
+
+
+
+url = 'https://webmaster-tools.xvideos.com/xvideos.com-export-week.csv.zip'
+
+def retrive_from_site(url):
+    file_name = "xvid.zip"
+    
+    buffer = get(url, allow_redirects=True)
+    open(file_name, 'wb').write(buffer.content)
+
+    with ZipFile(file_name, 'r') as zip:
+        zip.extractall()
+
+    remove(file_name)
+  
+
+def convert_to_dictionary():
+    #checks wrther the file exsts and retrive zip file from site and extracts it
+    file_name = 'xvideos.com-export-week.csv'
+    if not path.isfile(file_name):
+        retrive_from_site(url)
+    i=0
+    with open(file_name,'r', encoding='utf-8') as f:
+        value = reader(f)
+        with open('links.txt', 'w') as fl:
+            fl.write('{')
+            for val in value:
+                link = search('^[A-Za-z:\/.0-9_-]*', val[0]).group()
+                i+=1
+                #Use \n if you want multiline  
+                fl.write(str(i) + ':\'' + link +'\', ')     
+                #appends to a text file as a dictionary
+            fl.write('}')
+    remove(file_name)
+
+
+def fetch():
+
+    if not (path.isfile('links.txt')):
+        convert_to_dictionary()
+
+    with open('links.txt', 'r', encoding='utf-8') as f:
+        dic= f.read()
+    dictionary = ast.literal_eval(dic)
+    vid = dictionary[randint(0,63614)]
+    return(vid)
+
+
+def fetch_refresh():
+    convert_to_dictionary()
+    return(fetch())
+    
+
+
 
 
 async def new_join_f(client, message):
@@ -47,13 +110,13 @@ async def rename_message_f(client, message):
     inline_keyboard = []
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
-            text="read this?",
-            url="https://t.me/keralagram/698909"
+            text="download?",
+            await message.reply_text("/ytdl@ieat1337bot", quote=True)
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "please use @renamebot",
+        "{}".format(fetch()),
         quote=True,
         reply_markup=reply_markup
     )
